1a658be2da97021076b35eeef0ec5f0aef87e5ac
beopiyo <benardopiyo13@gmail.com>
2024-08-15 15:01:14 +0300

    fix: formatting

--- pkg/api/FetchLocations.go
+++ pkg/api/FetchLocations.go
@@ -3,22 +3,21 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"groupie-Tracker/pkg/models"
 	"net/http"
+
+	"groupie-Tracker/pkg/models"
 )
 
 func FetchLocations() ([]models.Location, error) {
-    resp, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
-    if err != nil {
-        return nil, fmt.Errorf("failed to fetch locations: %v", err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch locations: %v", err)
+	}
+	defer resp.Body.Close()
 
-    var wrapper models.LocationWrapper
-    if err := json.NewDecoder(resp.Body).Decode(&wrapper); err != nil {
-        return nil, fmt.Errorf("failed to decode locations: %v", err)
-    }
-
-    return wrapper.Index, nil
+	var wrapper models.LocationWrapper
+	if err := json.NewDecoder(resp.Body).Decode(&wrapper); err != nil {
+		return nil, fmt.Errorf("failed to decode locations: %v", err)
+	}
+	return wrapper.Index, nil
 }
-
