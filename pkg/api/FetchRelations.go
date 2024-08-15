0ab880b7cbeedd11b02c98b5a19eaa637f5d4380
beopiyo <benardopiyo13@gmail.com>
2024-08-15 15:01:24 +0300

    fix: formatting

--- pkg/api/FetchRelations.go
+++ pkg/api/FetchRelations.go
@@ -1,27 +1,23 @@
 package api
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
+
 	"groupie-Tracker/pkg/models"
 )
 
-type RelationsResponse struct {
-    Index []models.Relation `json:"index"`
-}
-
 func FetchRelations() ([]models.Relation, error) {
-    resp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
-    if err != nil {
-        return nil, fmt.Errorf("failed to fetch relations: %v", err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch relations: %v", err)
+	}
+	defer resp.Body.Close()
 
-    var response RelationsResponse
-    if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-        return nil, fmt.Errorf("failed to decode relations: %v", err)
-    }
-
-    return response.Index, nil
+	var response models.RelationsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, fmt.Errorf("failed to decode relations: %v", err)
+	}
+	return response.Index, nil
 }
