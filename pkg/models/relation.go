a43f6f41c5edcdb496f81a28da9af123776f91e5
beopiyo <benardopiyo13@gmail.com>
2024-08-15 15:02:26 +0300

    fix: formatting

--- pkg/models/relation.go
+++ pkg/models/relation.go
@@ -4,3 +4,7 @@ type Relation struct {
 	Id            int                 `json:"id"`
 	DatesLocation map[string][]string `json:"datesLocations"`
 }
+
+type RelationsResponse struct {
+    Index []Relation `json:"index"`
+}
