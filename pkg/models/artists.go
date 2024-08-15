8a02bbab51d33dd1e85c8f22559f25fcb144da9e
beopiyo <benardopiyo13@gmail.com>
2024-08-15 15:02:08 +0300

    fix: formatting

--- pkg/models/artists.go
+++ pkg/models/artists.go
@@ -1,13 +1,13 @@
 package models
 
 type Artist struct {
-	Id           int         `json:"id"`
-	Name         string      `json:"name"`
-	Image        string      `json:"image"`
-	Member       []string    `json:"members"`
-	CreationDate int         `json:"creationDate"`
-	FirstAlbum   string      `json:"firstAlbum"`
-	Location     string    `json:"locations"`
-	ConcertDate  string `json:"concertDates"`
-	Relation     string    `json:"relations"`
+	Id           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	Member       []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Location     string   `json:"locations"`
+	ConcertDate  string   `json:"concertDates"`
+	Relation     string   `json:"relations"`
 }
